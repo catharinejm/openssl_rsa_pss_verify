70f242d92931d454e028753501891d62ccc3b69c
Jon Distad <jon.distad@gmail.com>
2013-08-02 16:18:52 -0400

    add libcrypto check

--- ext/openssl_rsa_pss_verify/extconf.rb
+++ ext/openssl_rsa_pss_verify/extconf.rb
@@ -1,2 +1,7 @@
 require 'mkmf'
-create_makefile('openssl_rsa_pss_verify')
+
+if find_library("crypto", "EVP_PKEY_CTX_new", ENV["LD_LIBRARY_PATH"] || "")
+  create_makefile('openssl_rsa_pss_verify')
+else
+  raise "libcyrpto not found or too old!"
+end
