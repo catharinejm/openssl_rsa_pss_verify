0776589df76b433a95bfe0b14d2bc9d0b8d7b3c3
Jon Distad <jon.distad@gmail.com>
2013-08-02 16:51:22 -0400

    include openssl 1.0.1e

--- ext/openssl_rsa_pss_verify/extconf.rb
+++ ext/openssl_rsa_pss_verify/extconf.rb
@@ -1,9 +1,11 @@
 require 'mkmf'
 
-dir_config('', ENV["LD_LIBRARY_PATH"]||"", ENV["C_INCLUDE_PATH"]||"")
+openssl_path = File.expand_path("../../../vendor/openssl", __FILE__)
+
+dir_config('', File.join(openssl_path, "lib"), File.join(openssl_path, "include"))
 
 if have_const("RSA_PKCS1_PSS_PADDING", "openssl/rsa.h")
   create_makefile('openssl_rsa_pss_verify')
 else
-  raise "libcyrpto not found or too old!"
+  fail "libcyrpto not found or too old!"
 end
