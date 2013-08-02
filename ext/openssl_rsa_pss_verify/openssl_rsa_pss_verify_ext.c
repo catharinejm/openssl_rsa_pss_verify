4598ce102f931d4be8ce4389a631699703f50f3a
Jon Distad <jon.distad@gmail.com>
2013-08-02 11:35:13 -0400

    fix warnings, clearer naming

--- ext/openssl_rsa_pss_verify/openssl_rsa_pss_verify_ext.c
+++ ext/openssl_rsa_pss_verify/openssl_rsa_pss_verify_ext.c
@@ -11,22 +11,25 @@ static VALUE rb_mPKey;
 static VALUE rb_cRSA;
 static VALUE rb_cRSAError;
 
-VALUE __openssl_rsa_pss_verify(VALUE self, VALUE vSig, VALUE vData, VALUE vSaltLen) {
+VALUE openssl_rsa_pss_verify__verify_pss_sha1(VALUE self, VALUE vSig, VALUE vHashData, VALUE vSaltLen) {
   EVP_PKEY * pkey;
+  EVP_PKEY_CTX * pkey_ctx;
   int verify_rval;
 
   StringValue(vSig);
-  StringValue(vData);
+  StringValue(vHashData);
 
   Data_Get_Struct(self, EVP_PKEY, pkey);
-  EVP_PKEY_CTX * pkey_ctx = EVP_PKEY_CTX_new(pkey, ENGINE_get_default_RSA());
+  pkey_ctx = EVP_PKEY_CTX_new(pkey, ENGINE_get_default_RSA());
 
   EVP_PKEY_verify_init(pkey_ctx);
   EVP_PKEY_CTX_set_signature_md(pkey_ctx, EVP_sha1());
   EVP_PKEY_CTX_set_rsa_padding(pkey_ctx, RSA_PKCS1_PSS_PADDING);
   EVP_PKEY_CTX_set_rsa_pss_saltlen(pkey_ctx, NUM2INT(vSaltLen));
 
-  verify_rval = EVP_PKEY_verify(pkey_ctx, RSTRING_PTR(vSig), RSTRING_LEN(vSig), RSTRING_PTR(vData), RSTRING_LEN(vData));
+  verify_rval = EVP_PKEY_verify(pkey_ctx, 
+                                (unsigned char*)RSTRING_PTR(vSig), RSTRING_LEN(vSig), 
+                                (unsigned char*)RSTRING_PTR(vHashData), RSTRING_LEN(vHashData));
 
   EVP_PKEY_CTX_free(pkey_ctx);
 
@@ -49,5 +52,5 @@ void Init_openssl_rsa_pss_verify() {
   rb_cRSA = rb_const_get_at(rb_mPKey, rb_intern("RSA"));
   rb_cRSAError = rb_const_get_at(rb_mPKey, rb_intern("RSAError"));
 
-  rb_define_method(rb_cRSA, "verify_pss", __openssl_rsa_pss_verify, 3);
+  rb_define_method(rb_cRSA, "verify_pss_sha1", openssl_rsa_pss_verify__verify_pss_sha1, 3);
 }
