9c7872f64f26b4a08ad3f7050817ce00b95e673e
Jon Distad <jon.distad@gmail.com>
2013-08-05 11:30:07 -0400

    remove openssl

--- ext/openssl_rsa_pss_verify/extconf.rb
+++ ext/openssl_rsa_pss_verify/extconf.rb
@@ -1,13 +1,5 @@
 require 'mkmf'
 
-openssl_path = File.expand_path("../../../vendor/openssl", __FILE__)
-
-if RUBY_PLATFORM =~ /linux/
-  $LDFLAGS << "-L#{openssl_path}/lib"
-  $LDFLAGS << "-lcrypto"
-  $CFLAGS << "-I#{openssl_path}/include"
-end
-
 if have_const("RSA_PKCS1_PSS_PADDING", "openssl/rsa.h")
   create_makefile('openssl_rsa_pss_verify')
 else
