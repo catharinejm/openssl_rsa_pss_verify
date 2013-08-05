e304a428ca3912559a74be3890ddcd2a4464b720
Jon Distad <jon.distad@gmail.com>
2013-08-05 10:36:17 -0400

    Different vars

--- ext/openssl_rsa_pss_verify/extconf.rb
+++ ext/openssl_rsa_pss_verify/extconf.rb
@@ -3,8 +3,9 @@ require 'mkmf'
 openssl_path = File.expand_path("../../../vendor/openssl", __FILE__)
 
 if RUBY_PLATFORM =~ /linux/
-  ENV['LDCONFIG']="-L#{openssl_path}/lib -lcrypto"
-  ENV['CFLAGS']="-I#{openssl_path}/include"
+  $LDFLAGS << "-L#{openssl_path}/lib"
+  $LDFLAGS << "-lcrypto"
+  $CFLAGS << "-I#{openssl_path}/include"
 end
 
 if have_const("RSA_PKCS1_PSS_PADDING", "openssl/rsa.h")
