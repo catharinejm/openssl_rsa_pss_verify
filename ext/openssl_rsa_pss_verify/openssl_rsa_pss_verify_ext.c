0bd4b0a20f20bfe7034cfb180ee3fd345738e7f2
Jon Distad <jon.distad@gmail.com>
2013-08-05 18:48:01 -0400

    Move NUM2INT() before EVP_PKEY_CTX_new()
    Previously, if NUM2INT raised an exception, the call to
    EVP_PKEY_CTX_free() would be missed, leaking the CTX memory.

--- ext/openssl_rsa_pss_verify/openssl_rsa_pss_verify_ext.c
+++ ext/openssl_rsa_pss_verify/openssl_rsa_pss_verify_ext.c
@@ -14,10 +14,11 @@ static VALUE rb_cRSAError;
 VALUE openssl_rsa_pss_verify__verify_pss_sha1(VALUE self, VALUE vSig, VALUE vHashData, VALUE vSaltLen) {
   EVP_PKEY * pkey;
   EVP_PKEY_CTX * pkey_ctx;
-  int verify_rval;
+  int verify_rval, salt_len;
 
   StringValue(vSig);
   StringValue(vHashData);
+  salt_len = NUM2INT(vSaltLen);
 
   Data_Get_Struct(self, EVP_PKEY, pkey);
   pkey_ctx = EVP_PKEY_CTX_new(pkey, ENGINE_get_default_RSA());
@@ -25,7 +26,7 @@ VALUE openssl_rsa_pss_verify__verify_pss_sha1(VALUE self, VALUE vSig, VALUE vHas
   EVP_PKEY_verify_init(pkey_ctx);
   EVP_PKEY_CTX_set_signature_md(pkey_ctx, EVP_sha1());
   EVP_PKEY_CTX_set_rsa_padding(pkey_ctx, RSA_PKCS1_PSS_PADDING);
-  EVP_PKEY_CTX_set_rsa_pss_saltlen(pkey_ctx, NUM2INT(vSaltLen));
+  EVP_PKEY_CTX_set_rsa_pss_saltlen(pkey_ctx, salt_len);
 
   verify_rval = EVP_PKEY_verify(pkey_ctx, 
                                 (unsigned char*)RSTRING_PTR(vSig), RSTRING_LEN(vSig), 
